7ccb4242a71ead4cbec9a447f6c3ccfaa17efc9b
rigidrock <hjuang007@gmail.com>
2015-05-11 09:13:50 +0800

    Create fmtvul.c

---
+++ 4.Format-string/fmtvul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#define MAX 256
+
+int main()
+{
+  fmt();
+}
+
+int fmt()
+{
+  int n;
+  char str[MAX];
+  n=read(0, str, MAX-1);
+  str[n]='\x0';
+  printf(str);
+  return;
+}
